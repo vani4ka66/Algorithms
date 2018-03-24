983378906304573f3cc63305e18ef90735bf2d1f
Vania Dimitrova <vani4ka66@abv.bg>
2018-03-24 22:37:11 +0200

    Create 04. Variations with Repetition.cs

---
+++ Homeworks/03. Combinatorial-Algorithm/01. Combinatorial-Algorithms-Lab/04. Variations with Repetition.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        private static HashSet<string> set = new HashSet<string>();
+
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            int k = int.Parse(Console.ReadLine());
+
+            string[] arr = new string[k];
+            Gen(input, arr, 0, 0);
+        }
+
+        public static void Gen(string[] input, string[] arr, int index, int border)
+        {
+            if (index > arr.Length - 1)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+            }
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                { 
+                    arr[index] = input[i];
+                    Gen(input, arr, index + 1, i + 1);
+                }
+            }
+           
+        }
+    }
+}
